8d96ec992d999d92950e12bfe9519db89b7e216d
MarsTyrant <chiritamarius818@gmail.com>
2024-03-01 16:08:58 +0100

    Changes and new JS file

---
+++ script.js
@@ -0,0 +1,15 @@
+const button1=document.querySelector("#button1");
+const button2=document.querySelector("#button2");
+
+const bioTexts=
+[
+    {
+        text : "I am from the city of Chișinău, Republic of Moldova\nI am 20 years old.\nI am currently an university student at the University of Southern Denmark in the city of Sønderborg.\n I am studying Software Engineering and I am in my 2nd semester.\nSome of my passions are music, programming and reading."
+    },
+    {
+        text : "Intelligent, Curious, Hard-Working, Kind-Hearted\nCooperative, Detail-Oriented, Rational, Open-minded"
+    }
+
+
+
+]
